213d8f4214b93e6695be8b347d9c94ec96110fc3
Ricardo Atalla <ricardoatallastudent@gmail.com>
2025-08-29 01:12:19 -0400

    Dark mode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,81 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import aboutme from './assets/aboutme.png'
-import dark from './assets/dark.png'
+import moon from './assets/dark.png'
 import dropdown from './assets/dropdown.png'
 import projects from './assets/projects.png'
 import socials from './assets/socials.png'
+import darkaboutme from './assets/darkaboutme.png'
+import darkmoon from './assets/darksun.png'
+import darkdropdown from './assets/darkdropdown.png'
+import darkprojects from './assets/darkprojects.png'
+import darksocials from './assets/darksocials.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pageState, setPageState] = useState(0)
+  const [dark, setDark] = useState(false)
+
+  useEffect(() => {
+    if (dark) {
+      document.body.classList.add("dark")
+    } else {
+      document.body.classList.remove("dark")
+    }
+  }, [dark])
 
   return (
     <>
-      <div class="bar-wrap">
-        <div class="bar">
-          <button class="dropdown"><img src={dropdown} alt="Dropdown" /></button>
-          <button class="aboutme"><img src={aboutme} alt="About Me" /></button>
-          <button class="socials"><img src={socials} alt="Socials" /></button>
-          <button class="projects"><img src={projects} alt="Projects" /></button>
-          <button class="dark"><img src={dark} alt="Dark Mode" /></button>
+      <div className="bar-wrap">
+        <div className="bar">
+          <button className="dropdown">
+            <img src={dark ? darkdropdown : dropdown} alt="Dropdown" />
+          </button>
+          <button className="aboutme">
+            <img src={dark ? darkaboutme : aboutme} alt="About Me" />
+          </button>
+          <button className="socials">
+            <img src={dark ? darksocials : socials} alt="Socials" />
+          </button>
+          <button className="projects">
+            <img src={dark ? darkprojects : projects} alt="Projects" />
+          </button>
+          <button className="dark" onClick={() => setDark(prev => !prev)}>
+            <img src={dark ? darkmoon : moon} alt="Dark Mode" />
+          </button>
         </div>
       </div>
-        <div class="bar-wrapper">
-          <div class="bar-design1">
+        <div className="bar-wrapper">
+          <div className="bar-design1">
 
           </div>
-          <div class="bar-circle1">
+          <div className="bar-circle1">
 
           </div>
-          <div class="bar-design2">
+          <div className="bar-design2">
 
           </div>
-          <div class="bar-circle2">
+          <div className="bar-circle2">
 
           </div>
-          <div class="bar-design3">
+          <div className="bar-design3">
 
           </div>
-          <div class="bar-circle3">
+          <div className="bar-circle3">
 
           </div>
 
       </div>
-      <div class="app">
-        <div class="animation">
-          <div class="app">
-            <div class="loading-wrapper">
-              <div class="loading">
+      <div className="app">
+        <div className="animation">
+          <div className="app">
+            <div className="loading-wrapper">
+              <div className="loading">
 
               </div>
             </div>
           </div>
 
-          <div class="circle">
+          <div className="circle">
 
           </div>
         </div>
