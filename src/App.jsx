7199d49c5a0188163a28f02675485f2cb752dc4c
Ricardo Atalla <ricardoatallastudent@gmail.com>
2025-11-01 18:33:57 -0400

    all functionality complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ import darkdropdownup from './assets/darkdropdownup.png'
 import darkprojects from './assets/darkprojects.png'
 import darksocials from './assets/darksocials.png'
 import face from './assets/face.png'
+import tile from './assets/Tiles.png'
+import slime from './assets/slime.png'
+import powder from './assets/powder.png'
+import soon from './assets/soon.png'
 import './App.css'
 
 function App() {
@@ -32,7 +36,7 @@ function App() {
       case 2:
         return <div>
           <img src={face} className="face"/>
-          <p className="text-xl text-center">Hi, I'm Ricardo! Thanks for stopping by my portfolio! 
+          <p className="AboutText">Hi, I'm Ricardo! Thanks for stopping by my portfolio! 
         I'm currently a second year computer science student at the University of Ottawa. 
         I hope to work in cybersecurity, but as of now, I work on various side projects.</p>
         <p>Coding has always been a passion of mine. 
@@ -40,19 +44,34 @@ function App() {
           Take a look at them in the projects tab!</p></div>
         
       case 3:
-        return <div><h1><b>SOCIALS</b></h1><p></p><p className="text-xl text-center">You can contact me by sending me a DM on Instagram <a href="https://www.instagram.com/rickyatalla?igsh=MXNtNGc0Ync5dmg1dw%3D%3D&utm_source=qr">@rickyatalla</a></p>
+        return <div><h1><b>SOCIALS</b></h1><p></p><p className="SocialsText">You can contact me by sending me a DM on Instagram <a href="https://www.instagram.com/rickyatalla?igsh=MXNtNGc0Ync5dmg1dw%3D%3D&utm_source=qr">@rickyatalla</a></p>
         <p>Learn more about me on my LinkedIn <a href="https://www.linkedin.com/in/ricardo-atalla-2b0837343/">@ricardo-atalla</a></p>
         <p>Take a look or try out some of my projects on my GitHub <a href="https://github.com/RickyProjects">@RickyProjects</a></p>
           </div>
       case 4:
-        return <div classNamae="ProjectsTitle">
-          <p>If you'd like to try out any of my projects, take a look at my <a href="https://github.com/RickyProjects">GitHub</a>!</p>
-          <div clasName = "ProjectsList"></div>
-          <p className="text-xl text-center">Twirling Tiles</p>
-          <p className="text-xl text-center">Slimy Skirmish</p>
-          <p className="text-xl text-center">Art Portfolio</p>
-          <p className="text-xl text-center">Coming Soon!</p>
+        return <div className="ProjectsTitle">
+          <p>If you'd like to try out any of my other projects, take a look at my <a href="https://github.com/RickyProjects">GitHub</a>!</p>
+
+          <div className="ProjectsList">
+            <div>
+              <p className="TwirlingTiles"><a href="https://github.com/RickyProjects/Twirling-Tiles-Terrain">Twirling Tiles</a>: Creates a 3 dimensional terrain with a selectable grid size. Blocks are breakable and placeable by clicking circles. </p>
+              <img src={tile} className="tile" />
+            </div>
+            <div>
+              <p className="SlimySkirmish"><a href="https://github.com/RickyProjects/Slimy-Skirmish">Slimy Skirmish</a>: A 2D story and adventure video game that follows the story of slime lost in a barren wasteland of ruins.</p>
+              <img src={slime} className="slime" />
+            </div>
+            <div>
+              <p className="PowderSimulation">Powder Simulation: A simulation of how different powdered materials interact using a sandbox format (in progress).</p>
+              <img src={powder} className="powder" />
+            </div>
+            <div>
+              <p className="ComingSoon">Many more projects are in the works. Check back in a few months and I'm certain many more projects will be here!</p>
+              <img src={soon} className="soon" />
+            </div>
           </div>
+        </div>
+
       default:
         return <p className="text-xl text-center"></p>
     }
